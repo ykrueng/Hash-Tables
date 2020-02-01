c3835bcb0c07decb348322febd1d4139c6ad6fbc
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-01-31 21:00:09 -0600

    implement hash table + stretch

--- src/hashtable.py
+++ src/hashtable.py
@@ -16,7 +16,6 @@ class HashTable:
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
 
-
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
@@ -25,23 +24,23 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
-
+        hash = 5381
+        for x in key:
+            hash = (( hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
 
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
-
+        return self._hash_djb2(key) % self.capacity
 
     def insert(self, key, value):
         '''
@@ -51,9 +50,11 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
-
+        index = self._hash_mod(key)
+        newLinkedPair = LinkedPair(key, value)
+        if self.storage[index] is not None:
+            newLinkedPair.next = self.storage[index]
+        self.storage[index] = newLinkedPair
 
     def remove(self, key):
         '''
@@ -63,8 +64,22 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        found = False
 
+        if self.storage[index] is not None:
+            pointer = self.storage[index]
+
+            if pointer is not None and pointer.key == key:
+                self.storage[index] = pointer.next
+            else:
+                while pointer.next is not None and not found:
+                    if pointer.next.key == key:
+                        pointer.next = pointer.next.next
+                        found = True
+                    pointer = pointer.next
+        if not found:
+            print(f'Cannot find key: {key}')
 
     def retrieve(self, key):
         '''
@@ -74,8 +89,17 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        value = None
+        if self.storage[index] is not None:
+            pointer = self.storage[index]
 
+            while pointer is not None and value is None:
+                if pointer.key == key :
+                    value = pointer.value
+                else:
+                    pointer = pointer.next
+        return value
 
     def resize(self):
         '''
@@ -84,10 +108,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity *= 2
+        oldStorage = self.storage
+        self.storage = [None] * self.capacity
 
-
-
+        for linkedPair in oldStorage:
+            pointer = linkedPair
+            while pointer is not None:
+                self.insert(pointer.key, pointer.value)
+                pointer = pointer.next
+        
 if __name__ == "__main__":
     ht = HashTable(2)
 
